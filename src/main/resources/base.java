1041c240169ab59cab92ef53572f86303ef175ff
Carlos Kirui <carloskirui154@gmail.com>
2023-10-19 06:36:54 -0400

    updated code with ecommerce tests sequences

--- src/main/resources/base.java
+++ src/main/resources/base.java
@@ -1,39 +1,65 @@
 package main.resources;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class base {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\carlo\\Documents\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         // expected
-        String[] itemsNeeded = {"Cucumber", "Broccoli","Beetroot"};
+        String[] itemsNeeded = {"Cucumber","Brocolli", "Beetroot", "Carrot"};
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 
-        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+        addItems(driver, itemsNeeded);
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promobtn")).click();
 
-        for (int i = 0; i <products.size();i++){
-            String name = products.get(i).getText();
+    }
+
+
+
+
+
+
+
+    public static void addItems(WebDriver driver, String[] itemsNeeded) {
+        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+        int j = 0;
+
+        for (int i = 0; i < products.size(); i++) {
+
+            String[] name = products.get(i).getText().split("-");
+            String formattedName = name[0].trim();
             // check if extracted item present in array
             // convert from array to ArrayList for easier search
 
             List<String> itemsNeededList = Arrays.asList(itemsNeeded);
 
-            if(itemsNeededList.contains(name)) {
+            if (itemsNeededList.contains(formattedName)) {
                 // Add to Cart logic
-                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-                break;
+                j++;
+                System.out.println(j);
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
 
+                if(j == itemsNeededList.size()){
+                    System.out.println("Iterated over " + itemsNeededList.size());
+                    break;
+                }
             }
         }
     }
+
 
 }
