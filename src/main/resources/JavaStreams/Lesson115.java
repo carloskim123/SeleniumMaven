d61b81ffaadf0429d79debe957e6875987e86f94
Carlos Kirui <carloskirui154@gmail.com>
2024-07-13 16:31:59 -0400

    reviewed some files

--- src/main/resources/JavaStreams/Lesson115.java
+++ src/main/resources/JavaStreams/Lesson115.java
@@ -3,6 +3,8 @@ package main.resources.JavaStreams;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Lesson115 {
@@ -69,12 +71,24 @@ public class Lesson115 {
     @Test
     public void streamMap() {
 
+        ArrayList<String> names = new ArrayList<String>();;
+
         // print names which have last letter as "a" in Uppercase
 
 
-        Stream.of("Abhijeet", "Don", "Alekhya", "Arnold", "Adam", "Rama")
+        /* Stream.of("Abhijeet", "Don", "Alekhya", "Arnold", "Adam", "Rama")
                 .filter(s -> s.endsWith("a"))
-                .map(m -> m.toUpperCase()).forEach(System.out::println);
+                .map(m -> m.toUpperCase()).forEach(System.out::println); */
+
+        // print names which have first letter as "a" in Uppercase and sorted
+
+        List<String> names1 = Arrays.asList("Abhijeet", "Don", "Alekhya", "Arnold", "Adam", "Rama");
+        names1.stream().filter(s -> s.startsWith("A")).sorted().map(m -> m.toUpperCase()).forEach(System.out::println);
+
+        // merging two streams
+        Stream<String> newStream = Stream.concat(names.stream(), names1.stream());
+        newStream.sorted().forEach(System.out::println);
+
     }
 
 
